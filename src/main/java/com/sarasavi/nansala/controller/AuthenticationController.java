9e5e2ec8e07e9e279e1f99e28277e32d9535dd9d
Oshada9319 <oshadamahee@gmail.com>
2023-08-27 17:21:40 +0530

    import optimize

--- src/main/java/com/sarasavi/nansala/controller/AuthenticationController.java
+++ src/main/java/com/sarasavi/nansala/controller/AuthenticationController.java
@@ -1,20 +1,16 @@
 package com.sarasavi.nansala.controller;
 
-import com.google.gson.JsonObject;
-import com.google.gson.JsonParser;
 import com.sarasavi.nansala.model.AuthenticationRequest;
 import com.sarasavi.nansala.model.AuthenticationResponse;
-import com.sarasavi.nansala.model.Book;
 import com.sarasavi.nansala.model.RegisterRequest;
 import com.sarasavi.nansala.service.AuthenticationService;
-import com.sarasavi.nansala.service.BookService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1/auth")
--- src/main/java/com/sarasavi/nansala/service/AuthenticationService.java
+++ src/main/java/com/sarasavi/nansala/service/AuthenticationService.java
@@ -1,20 +1,13 @@
 package com.sarasavi.nansala.service;
 
 import com.sarasavi.nansala.model.*;
-import com.sarasavi.nansala.repository.BookRepository;
 import com.sarasavi.nansala.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
-import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
